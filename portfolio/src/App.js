654393603bdf240d170413523592e071418eabbd
Christian Gomez Martinez <gomez9289@gmail.com>
2020-07-05 11:19:23 -0400

    set up files and app.js

---
+++ portfolio/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/Projects" component={Projects} />
+      <Footer />
+    </div>
+    </Router>
+  );
+}
+
+export default App;
