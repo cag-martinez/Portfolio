afdcec95efbda73c02560733585c359116419a5d
Christian Gomez Martinez <gomez9289@gmail.com>
2020-09-14 19:23:02 -0400

    changed contact information to icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,22 +10,33 @@ function Home() {
           <img src={picture} className="card-img" alt="..." />
           <div className="card-body">
             <h3>Hi! My name is Christian</h3>
-            <p className="card-text">
-              I'm a full-stack web developer in the Charlotte metro area, NC.
-            </p>
-            <div className="card-header">Contact:</div>
-            <div className="list-group list-group-flush">
-              <a href="https://github.com/cag-martinez">Github</a>
-              <a href="https://www.linkedin.com/in/christian-gomez-martinez-0b06241aa/">
-                LinkedIn
+            <p className="card-text">I'm a full-stack web developer.</p>
+            <div className="card-header">Contact</div>
+            <div className="group ">
+              <a className="gitHub m-2" href="https://github.com/cag-martinez">
+                <i className="fab fa-github "></i>
               </a>
-              <a href="https://docs.google.com/document/d/18qw-lEY3oQNshFULbmP3Z0aQER8ksWDaQ9U3hTic1Kk/edit?usp=sharing">
-                Resume
+              <a
+                className="linkedIn m-2"
+                href="https://www.linkedin.com/in/christian-gomez-martinez-0b06241aa/"
+              >
+                <i className="fab fa-linkedin-in"></i>
               </a>
-              <a href="https://infinite-wildwood-08615.herokuapp.com/Contact">
+              <a
+                className="resume m-2"
+                href="https://docs.google.com/document/d/18qw-lEY3oQNshFULbmP3Z0aQER8ksWDaQ9U3hTic1Kk/edit?usp=sharing"
+              >
+                <i className="far fa-file"></i>
+              </a>
+              <a
+                className="email m-2"
+                href="https://infinite-wildwood-08615.herokuapp.com/Contact"
+              >
                 {" "}
-                cag.martinez20@gmail.com
+                <i className="fas fa-envelope-open-text"></i>
               </a>
+            </div>
+            <div className="list-group ">
               <a>704-603-7988</a>
             </div>
           </div>
@@ -42,14 +53,15 @@ function Home() {
           <p className="lead">
             I am an outdoor enthusiast, foodie and a solutions-driven web
             developer with a passion for learning and helping others. Based in
-            Charlotte NC. There is a lot that i have learned and experienced in my professional 
-            career. I have been a customer acquisition and relationships manager
-            at a bank and spent four years working at a top-level country club
-            as a sous chef leading a fantastic team. I have participated in two
-            culinary competitons and also have participated in culinary events
-            for non-profit organizations such as JDRF. Although fascinating, the most
-            exhilarating moments are those when i am in front of my coputer
-            writing code and making user friendly websites.
+            Charlotte NC. There is a lot that i have learned and experienced in
+            my professional career. I have been a customer acquisition and
+            relationships manager at a bank and spent four years working at a
+            top-level country club as a sous chef leading a fantastic team. I
+            have participated in two culinary competitons and also have
+            participated in culinary events for non-profit organizations such as
+            JDRF. Although fascinating, the most exhilarating moments are those
+            when i am in front of my coputer writing code and making user
+            friendly websites.
             {/* My skills include HTML5, CSS3, JavaScript, Reactjs, Nodejs, MongoDB,
             Mysql, OOP, MVC, API's, Git, CLI and visual studio code. With the
             knowledge I have acquired from the full stack web development
