17ee1e693029220bdb0d9f311720efcd1a1a2e1f
Christian Gomez Martinez <gomez9289@gmail.com>
2020-12-01 16:14:03 -0500

    moved the styled for the header from css file to the Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,23 @@
 import React from "react";
-import Background from "../../assets/images/magnifyingCode.png"
+//import Background from "../../assets/images/headerImage.png"
 import "./header.css";
 
 function Header() {
   return (
     <div>
-      <section className="header" img={Background}></section>
-    </div>
+      <section className="header" style={{
+        height: "50vh",
+        backgroundImage: "url(https://www.uplers.com/wp-content/uploads/2020/05/Essential-Skills-to-look-for-when-you-Hire-Front-end-Developers.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        opacity: "75%",
+        
+        clipPath: "polygon(100% 0, 100% 75%, 69% 100%, 0 85%, 0 0)",
+        
+        backgroundAttachment: "fixed",
+      }}></section>
+    </div> 
   );
 }
 
