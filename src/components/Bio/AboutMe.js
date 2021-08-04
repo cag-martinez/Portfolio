5e1a1c633abcde7adab96897b02505e5e2cfa4e1
Christian Gomez Martinez <gomez9289@gmail.com>
2021-08-03 22:37:15 -0500

    Added the card that will contain the information in the aboutMe component and added some simple styling to get an idea of where it is and how it should look

--- src/components/Bio/AboutMe.js
+++ src/components/Bio/AboutMe.js
@@ -1,12 +1,32 @@
-import React from 'react';
-//import './aboutMe.css';
+import React from "react";
+import picture from "../../assets/images/suit.png";
+import './aboutMe.css';
 
-function AboutMe (){
-        return(
-            <div>
-                This will be a card displaying the bio and profile picture
+function AboutMe() {
+  return (
+    <div>
+      <div className="aboutMeCard card mb-3" style={{maxWwidth: '540px'}}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={picture} className="img-fluid rounded-start" style={{maxBlockSize: '250px'}} alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
             </div>
-        )
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutMe;
