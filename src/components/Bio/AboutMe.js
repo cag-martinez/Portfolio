60faaed138400dc6c495e9a82aac75f86ca5e30d
Christian Gomez Martinez <gomez9289@gmail.com>
2021-08-04 22:29:54 -0500

    Background color in the AboutMe compomponent and also added some text

--- src/components/Bio/AboutMe.js
+++ src/components/Bio/AboutMe.js
@@ -4,28 +4,28 @@ import './aboutMe.css';
 
 function AboutMe() {
   return (
-    <div>
-      <div className="aboutMeCard card mb-3" style={{maxWwidth: '540px'}}>
+    
+      <div className="aboutMeCard card mb-3" style={{minWidth: 'inherit'}}>
         <div className="row g-0">
           <div className="col-md-4">
             <img src={picture} className="img-fluid rounded-start" style={{maxBlockSize: '250px'}} alt="..." />
           </div>
           <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+            <div className="aboutMeText card-body">
+              <h5 className="card-title">A little about me</h5>
               <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+              I am an outdoor enthusiast, foodie and a solutions-driven web
+                  developer with a passion for learning and helping others.
+                  Based in Charlotte NC. 
               </p>
-              <p className="card-text">
+              {/* <p className="card-text">
                 <small className="text-muted">Last updated 3 mins ago</small>
-              </p>
+              </p> */}
             </div>
           </div>
         </div>
       </div>
-    </div>
+    
   );
 }
 
